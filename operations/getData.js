bbff4a8ef53f474dcffa73c5d566a4bd79d8a2d7
Alisson Rodrigues <68715629+ar-rodrigues@users.noreply.github.com>
2022-10-10 19:42:56 +0000

    1.12

--- operations/getData.js
+++ operations/getData.js
@@ -2,7 +2,9 @@ const axios = require('axios');
 const fs = require('fs');
 
 const urlExistsPromised = require('./urlExistsPromised.js');
-require('dotenv').config()
+require('dotenv').config();
+const nextProducts = require('../public/nextProducts.json');
+
 
 const customer = process.env.CUSTOMER
 const key = process.env.KEY
@@ -13,72 +15,81 @@ const url = "https://pchm.to-do.mx/extcust/getprodlist/"
 
 
 const getData = async () => {
-  console.log("GETTING PRODUCTS")
+  console.log("CHEKING IF IS UPDATED")
 
-  try {
-    // Get data from de API
-    const res = await axios.post(url, {
-      "customer": customer, "key": key
-    })
+  const today = new Date().toDateString();
+  if (nextProducts.fecha == today) {
+    return nextProducts;
+  } else {
+    console.log("GETTING PRODUCTS")
+    try {
+      // Get data from de API
+      const res = await axios.post(url, {
+        "customer": customer, "key": key
+      })
 
-    const data = await res.data;
-    console.log(data.status)
+      const data = await res.data;
+      console.log(data.status)
 
-    fs.writeFile(
-      "public/productos.json",
-      JSON.stringify({
-        ...data, fecha: new Date().toDateString()
-      }), err => { err ? console.log(err) : console.log("productos.json created") })
+      fs.writeFile(
+        "public/productos.json",
+        JSON.stringify({
+          ...data, fecha: new Date().toDateString()
+        }), err => { err ? console.log(err) : console.log("productos.json created") })
 
-    if (await data.status != "200") {
-      return data;
-    } else {
-      const nextProductPromises = data.data.productos.map(async producto => {
+      if (await data.status != "200") {
+        return data;
+      } else {
+        const nextProductPromises = data.data.productos.map(async producto => {
 
-        let sku = producto.sku;
+          let sku = producto.sku;
 
-        // Possible URL that work
-        let pchUrls = [
-          `https://www.pchmayoreo.com/pub/media/catalog/product/${sku.substring(0, 1)}/${sku.substring(1, 2)}/${sku}.jpg`,
-          `https://www.pchmayoreo.com/pub/media/catalog/product/${sku.substring(0, 1)}/${sku.substring(1, 2)}/${sku}.jpg`.toLowerCase(),
-          //`https://www.pchmayoreo.com/pub/media/catalog/product/${sku.substring(0, 1)}/${sku.substring(1, 2)}/${sku}-01.jpg`.toLowerCase(),
-          `https://www.pchmayoreo.com/pub/media/catalog/product/${sku.substring(0, 1)}/${sku.substring(1, 2)}/${sku}.png`,
-          `https://www.pchmayoreo.com/pub/media/catalog/product/${sku.substring(0, 1)}/${sku.substring(1, 2)}/${sku}.png`.toLowerCase(),
-          `https://www.pchmayoreo.com/pub/media/catalog/product/${sku.substring(0, 1)}/${sku.substring(1, 2)}/${sku}.jpeg`,
-          `https://www.pchmayoreo.com/pub/media/catalog/product/${sku.substring(0, 1)}/${sku.substring(1, 2)}/${sku}.jpeg`.toLowerCase()
-        ];
+          // Possible URL that work
+          let pchUrls = [
+            `https://www.pchmayoreo.com/pub/media/catalog/product/${sku.substring(0, 1)}/${sku.substring(1, 2)}/${sku}.jpg`,
+            `https://www.pchmayoreo.com/pub/media/catalog/product/${sku.substring(0, 1)}/${sku.substring(1, 2)}/${sku}.jpg`.toLowerCase(),
+            //`https://www.pchmayoreo.com/pub/media/catalog/product/${sku.substring(0, 1)}/${sku.substring(1, 2)}/${sku}-01.jpg`.toLowerCase(),
+            `https://www.pchmayoreo.com/pub/media/catalog/product/${sku.substring(0, 1)}/${sku.substring(1, 2)}/${sku}.png`,
+            `https://www.pchmayoreo.com/pub/media/catalog/product/${sku.substring(0, 1)}/${sku.substring(1, 2)}/${sku}.png`.toLowerCase(),
+            `https://www.pchmayoreo.com/pub/media/catalog/product/${sku.substring(0, 1)}/${sku.substring(1, 2)}/${sku}.jpeg`,
+            `https://www.pchmayoreo.com/pub/media/catalog/product/${sku.substring(0, 1)}/${sku.substring(1, 2)}/${sku}.jpeg`.toLowerCase()
+          ];
 
-        // Iterate the possible URL to identify the one that work
-        for (let i = 0; i < pchUrls.length; i++) {
-          const url = pchUrls[i]
-          const exists = await urlExistsPromised(url);
+          // Iterate the possible URL to identify the one that work
+          for (let i = 0; i < pchUrls.length; i++) {
+            const url = pchUrls[i]
+            const exists = await urlExistsPromised(url);
 
 
-          if (exists) {
-            return {
-              ...producto,
-              imgUrl: url
+            if (exists) {
+              return {
+                ...producto,
+                imgUrl: url
+              }
             }
+          }// end of For
+
+          return {
+            ...producto,
+            imgUrl: ''
           }
-        }// end of For
+        })// end of map
 
-        return {
-          ...producto,
-          imgUrl: ''
-        }
-      })// end of map
-
-      const nextProducts = await Promise.all(nextProductPromises);
+        const nextProducts = await Promise.all(nextProductPromises);
 
 
-      fs.writeFile("public/nextProducts.json", JSON.stringify({ "status": 200, "fecha": new Date().toDateString(), "data": { "productos": nextProducts } }), err => { err ? console.log(err) : console.log("nextProducts.json created") })
+        fs.writeFile("public/nextProducts.json", JSON.stringify({ "status": 200, "fecha": new Date().toDateString(), "data": { "productos": nextProducts } }), err => { err ? console.log(err) : console.log("nextProducts.json created") })
 
-      return await { "status": 200, "fecha": new Date().toDateString(), "data": { "productos": nextProducts } };
-    }
+        return await { "status": 200, "fecha": new Date().toDateString(), "data": { "productos": nextProducts } };
+      }
 
-  } catch (e) {
-    console.error(`What?`, e);
-  }
+    } catch (e) {
+      console.error(`What?`, e);
+    }//end of try-catch
+
+  }//end of else
+
+
 }
 
 
